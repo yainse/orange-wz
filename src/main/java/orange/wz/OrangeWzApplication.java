01609ed3f9faa5f437d7e352fc06520c0791b3e4
leevccc <94272011+leevccc@users.noreply.github.com>
2025-11-26 19:25:58 +0800

    Initial SpringBoot 4.0.0

---
+++ src/main/java/orange/wz/OrangeWzApplication.java
@@ -0,0 +1,13 @@
+package orange.wz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrangeWzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrangeWzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/orange/wz/OrangeWzApplicationTests.java
@@ -0,0 +1,13 @@
+package orange.wz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrangeWzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
